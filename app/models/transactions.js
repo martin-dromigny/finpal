1d24bcbafba8ccb6bb51358e9e71f0e85cf954d4
Martin Dromigny <martin@altirnao.com>
2017-01-19 11:58:10 +0100

    main view

--- app/models/transactions.js
+++ app/models/transactions.js
@@ -1,13 +1,4 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  createdAt: DS.attr('string'),
-  counterpartyName: DS.attr('string'),
-  debit: DS.attr('string'),
-  credit: DS.attr('string'),
-  amount: DS.attr('string'),
-  currency: DS.attr('string'),
-  operation_type: DS.attr('string'),
-  // attachements : 
-
 });
